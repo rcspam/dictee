edcad8136e47b57f7d109f693d53c2911f6137ef
rcspam <rcspams@gmail.com>
2026-03-25 21:50:55 +0100

    chore: bump version 1.2.0~beta2

--- build-deb.sh
+++ build-deb.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")"
 
-VERSION="1.2.0~beta1"
+VERSION="1.2.0~beta2"
 PKG_DIR="pkg/dictee"
 
 DOTOOL_REPO="https://git.sr.ht/~geb/dotool"
@@ -118,7 +118,7 @@ build_cuda() {
     # Update control file for CUDA
     cat > "$PKG_DIR/DEBIAN/control" << 'EOF'
 Package: dictee-cuda
-Version: 1.2.0~beta1
+Version: 1.2.0~beta2
 Section: sound
 Priority: optional
 Architecture: amd64
@@ -176,7 +176,7 @@ build_cpu() {
     # Update control file for CPU
     cat > "$PKG_DIR/DEBIAN/control" << 'EOF'
 Package: dictee-cpu
-Version: 1.2.0~beta1
+Version: 1.2.0~beta2
 Section: sound
 Priority: optional
 Architecture: amd64
